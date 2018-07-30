6ac4392992e17ef0fe284bad7b40e00255ba5bfa
Mariel <marielk.mya@gmail.com>
2018-07-30 09:51:52 -0400

    creando funcion de firebase para guardar visitantes

--- docs/arch/src/js/controller.js
+++ docs/arch/src/js/controller.js
@@ -1,9 +1,33 @@
+//incializando firebase cloud firestore
+var firestore = firebase.firestore();
+const settings = { timestampsInSnapshots: true }; //activar registro de tiempo 
+firestore.settings(settings);
 
 window.currentVisitorRegistration = () => {
-  currentVisitor = {
-    name: 'mariel',
-    rut: 17834887-6,
-    arrivingTime: new Date
+
+  //crear id para cada visitante 
+  const newVisitorId = firestore.collection("visitors").ref().child('visitor').push().key;
+  const startedAt = firebase.collection("visitors").ServerValue.TIMESTAMP;
+  firestore.collection("visitors").onSnapshot((querySnapshot) => {
+          
+    querySnapshot.forEach((doc) => {
+      
+      let name = firestore.data().name;
+      //añadiendo una nueva coleccion
+      firestore.collection("visitor").add({
+        id: newVisitorId,
+        name : ,
+        rut:  ,
+        visitorType : ,
+        arrivingTime : ,
+        goingTo : ,
+        host : ,
+        licensePlate : 
+      }) 
+  
+  
+ 
+    }
+    return currentVisitor;
   }
-  return currentVisitor;
-};
+}
