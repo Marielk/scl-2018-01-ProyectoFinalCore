faeb81204349b43715fa3c5d791669f079812eda
Mariel <marielk.mya@gmail.com>
2018-07-31 09:00:42 -0400

    añadiendo funcion de login con google, instalacion de firebase-tools

--- docs/arch/src/js/controller.js
+++ docs/arch/src/js/controller.js
@@ -28,3 +28,41 @@ function validatePersonIdentity(){
 	 console.log(e);
 	});
 }
+
+// ==========Funciones avisar a la empresa========
+
+function singIn(){
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider).then(function (result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    let token = result.credential.accessToken;
+    // The signed-in user info.
+    let user = result.user;
+  })
+    .catch(function (error) {
+      console.log('entrar' + error);
+      // Handle Errors here.
+      let errorCode = error.code;
+      let errorMessage = error.message;
+      // The email of the user's account used.
+      let email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      let credential = error.credential;
+    });
+}
+
+function userOn(){
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      //Si estamos logueados 
+      const btnAceptNotifications = document.getElementById('aceptNotifications');
+      btnAceptNotifications.onclick = aceptNotifications();
+      } else {
+      // si no esta logeado no pasa nada 
+      }
+    });
+}
+
+function aceptNotifications(){
+  
+}
